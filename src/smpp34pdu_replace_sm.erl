c6e39dc1fa6262996890421bec18d40775caf7e2
Antarix <antarix.tandon@gmail.com>
2017-11-17 01:24:58 +0530

    Update smpp34pdu_replace_sm.erl

--- src/smpp34pdu_replace_sm.erl
+++ src/smpp34pdu_replace_sm.erl
@@ -5,8 +5,9 @@
 -import(pdu_data, [cstring_to_bin/2, string_to_bin/2, integer_to_bin/2]).
 -import(pdu_data, [bin_to_cstring/2, bin_to_string/2, bin_to_integer/2]).
 
--spec(pack/1 :: (replace_sm()) -> binary()).
--spec(unpack/1 :: (binary()) -> replace_sm()).
+
+-spec pack(replace_sm()) -> binary().
+-spec unpack(binary()) -> replace_sm().
 
 pack(#replace_sm{message_id=MessageId, 
 		source_addr_ton=SrcAddrTon,
