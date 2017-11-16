98efbad04ba8b366fca3b4fb238137e97aab28fd
Antarix <antarix.tandon@gmail.com>
2017-11-17 01:23:33 +0530

    Update smpp34pdu_submit_sm_resp.erl

--- src/smpp34pdu_submit_sm_resp.erl
+++ src/smpp34pdu_submit_sm_resp.erl
@@ -5,8 +5,8 @@
 -import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
 -import(pdu_data, [bin_to_cstring/2, bin_to_integer/2]).
 
--spec(pack/1 :: (submit_sm_resp()) -> binary()).
--spec(unpack/1 :: (binary()) -> submit_sm_resp()).
+-spec pack(submit_sm_resp()) -> binary().
+-spec unpack(binary()) -> submit_sm_resp().
 
 pack(#submit_sm_resp{message_id=MessageId}) ->
 		L = [cstring_to_bin(MessageId, 65)],
