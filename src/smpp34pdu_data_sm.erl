e1c2466d5d26616934a46cf2a802f2c91fc5cb27
Antarix <antarix.tandon@gmail.com>
2017-11-17 01:20:53 +0530

    Update smpp34pdu_data_sm.erl

--- src/smpp34pdu_data_sm.erl
+++ src/smpp34pdu_data_sm.erl
@@ -7,8 +7,10 @@
 -import(pdu_data, [cstring_to_bin/2, string_to_bin/2, integer_to_bin/2]).
 -import(pdu_data, [bin_to_cstring/2, bin_to_string/2, bin_to_integer/2]).
 
--spec(pack/1 :: (data_sm()) -> binary()).
--spec(unpack/1 :: (binary()) -> data_sm()).
+
+
+-spec pack(data_sm()) -> binary().
+-spec unpack(binary()) -> data_sm().
 
 pack(#data_sm{service_type=SrvType,
 		source_addr_ton=SrcAddrTon,
