5aca834610e781513019409933b056078afb43b7
Antarix <antarix.tandon@gmail.com>
2017-11-17 01:20:13 +0530

    Update smpp34pdu_data_sm_resp.erl

--- src/smpp34pdu_data_sm_resp.erl
+++ src/smpp34pdu_data_sm_resp.erl
@@ -6,8 +6,9 @@
 -import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
 -import(pdu_data, [bin_to_cstring/2, bin_to_integer/2]).
 
--spec(pack/1 :: (data_sm_resp()) -> binary()).
--spec(unpack/1 :: (binary()) -> data_sm_resp()).
+
+-spec pack(data_sm_resp()) -> binary().
+-spec unpack(binary()) -> data_sm_resp().
 
 pack(#data_sm_resp{message_id=MessageId,
 		delivery_failure_reason=DeliveryFailureReason,
