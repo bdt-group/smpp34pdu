fa8f76de0822ede1dc12c8aca705fbd9088dc5ff
Antarix <antarix.tandon@gmail.com>
2017-11-17 01:26:13 +0530

    Update smpp34pdu_query_sm.erl

--- src/smpp34pdu_query_sm.erl
+++ src/smpp34pdu_query_sm.erl
@@ -5,8 +5,9 @@
 -import(pdu_data, [cstring_to_bin/2, integer_to_bin/2]).
 -import(pdu_data, [bin_to_cstring/2, bin_to_integer/2]).
 
--spec(pack/1 :: (query_sm()) -> binary()).
--spec(unpack/1 :: (binary()) -> query_sm()).
+-spec pack(query_sm()) -> binary().
+-spec unpack(binary()) -> query_sm().
+
 
 pack(#query_sm{message_id=MessageId, 
 		source_addr_ton=SrcAddrTon,
