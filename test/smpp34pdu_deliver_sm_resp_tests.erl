d6707361482c3c4d2360e47d910604dc14e70d2e
Alexander Petrov <apetrov@bdt.group>
2022-10-14 17:15:56 +0300

    Fix tests

--- test/smpp34pdu_deliver_sm_resp_tests.erl
+++ test/smpp34pdu_deliver_sm_resp_tests.erl
@@ -2,8 +2,6 @@
 -include("../include/smpp34pdu.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
--record(deliver_sm_resp_custom_tlv, {message_id=?DEFAULT_CSTRING, custom_field=?DEFAULT_CSTRING}).
-
 deliver_sm_resp_test_() ->
     Payload = #deliver_sm_resp{message_id="abcdefghij"},
 
@@ -21,17 +19,3 @@ deliver_sm_resp_test_() ->
 			?_assertEqual(Bin,
 					smpp34pdu_deliver_sm_resp:pack(smpp34pdu_deliver_sm_resp:unpack(Bin)))}
 	].
-
-deliver_sm_resp_custom_tlv_test_() ->
-    Payload0 = #deliver_sm_resp_custom_tlv{message_id="abcdefghij", custom_field = "212312"},
-    Payload = #deliver_sm_resp{message_id="abcdefghij"},
-    Bin = pack(Payload0),
-
-    [
-		{"Unpacking Bin with custom TLV will give Payload without custom TLV",
-            ?_assertEqual(Payload, smpp34pdu_deliver_sm_resp:unpack(Bin))}
-    ].
-
-pack(#deliver_sm_resp_custom_tlv{message_id=MessageId, custom_field = CustomValue}) ->
-	L = [pdu_data:cstring_to_bin(MessageId, 65), pdu_data:cstring_to_bin(CustomValue, 65)],
-	list_to_binary(L).
